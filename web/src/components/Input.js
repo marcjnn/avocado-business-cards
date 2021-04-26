96fe97bf92ed52a869cc757245cd0af26cf12bca
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-04-26 11:13:57 +0200

    add props destructuring in Input.js

--- web/src/components/Input.js
+++ web/src/components/Input.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
 import "../style/layout/_form.scss";
 
 function Input(props) {
+  const { name, label, placeholder, value, type, handleInput } = props;
   const handleInputChange = (ev) => {
-    props.handleInput(ev);
+    handleInput(ev);
   };
 
   return (
     <>
-      <label className="form__label" htmlFor={props.name}>
-        {props.label}
+      <label className="form__label" htmlFor={name}>
+        {label}
       </label>
       <input
-        className={"form__input js-" + props.name + " js-input-text"}
-        type={props.type}
-        name={props.name}
-        id={props.name}
-        placeholder={props.placeholder}
-        value={props.value}
+        className={"form__input"}
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
         onChange={handleInputChange}
         required
       />
